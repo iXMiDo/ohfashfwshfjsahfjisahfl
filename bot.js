a80db7e97bbd0b654cffa622cbd7f6ec81df95eb
iXMiDo <41427808+iXMiDo@users.noreply.github.com>
2019-06-04 10:05:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,6 @@ const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
 const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
 
 
 
@@ -133,60 +130,6 @@ client6.on('message', message => {
         message.channel.send('#rep @Stewart#0005 ')
     }
 });
-client7.on('message', message => {
-    if(message.content === '!daily'){
-        message.channel.send('#daily')
-    }
-});
-
-client7.on('message', message => {
-    if(message.content === '!credits'){
-        message.channel.send('#credits')
-    }
-});
-
-client7.on('message', message => {
-    if(message.content === '!rep'){
-        message.channel.send('#rep @Stewart#0005 ')
-    }
-});
-client8.on('message', message => {
-    if(message.content === '!daily'){
-        message.channel.send('#daily')
-    }
-});
-
-client8.on('message', message => {
-    if(message.content === '!credits'){
-        message.channel.send('#credits')
-    }
-});
-
-client8.on('message', message => {
-    if(message.content === '!rep'){
-        message.channel.send('#rep @Stewart#0005 ')
-    }
-});
-client9.on('message', message => {
-    if(message.content === '!daily'){
-        message.channel.send('#daily')
-    }
-});
-
-client9.on('message', message => {
-    if(message.content === '!credits'){
-        message.channel.send('#credits')
-    }
-});
-
-client9.on('message', message => {
-    if(message.content === '!rep'){
-        message.channel.send('#rep @Stewart#0005 ')
-    }
-});
-
-
-
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
@@ -268,45 +211,9 @@ if (message.content === '!spam') {
         }
       }
 });
-client7.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-client8.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-client9.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
@@ -315,6 +222,3 @@ client3.login(process.env.BOT_TOKEN3);
 client4.login(process.env.BOT_TOKEN4);
 client5.login(process.env.BOT_TOKEN5);
 client6.login(process.env.BOT_TOKEN6);
-client7.login(process.env.BOT_TOKEN7);
-client8.login(process.env.BOT_TOKEN8);
-client9.login(process.env.BOT_TOKEN9);
