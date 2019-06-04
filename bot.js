ef0bf9e07e395ac666f45b453c4be8a20c10a71d
iXMiDo <41427808+iXMiDo@users.noreply.github.com>
2019-06-04 02:09:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+
+
+
+
+
+
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -69,8 +83,142 @@ client3.on('message', message => {
         message.channel.send('#rep @Stewart#0005 ')
     }
 });
+client4.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
 
+client4.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
 
+client4.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client5.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client6.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client7.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client8.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client9.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client10.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+client11.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client11.on('message', message => {
+    if(message.content === '!credits'){
+        message.channel.send('#credits')
+    }
+});
+
+client11.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
       let count = 0;
@@ -112,9 +260,119 @@ if (message.content === '!spam') {
         }
       }
 });
-
-
+client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client5.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client6.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client7.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client8.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client9.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client10.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+client11.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
 
 client.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN2);
 client3.login(process.env.BOT_TOKEN3);
+client4.login(process.env.BOT_TOKEN4);
+client5.login(process.env.BOT_TOKEN5);
+client6.login(process.env.BOT_TOKEN6);
+client7.login(process.env.BOT_TOKEN7);
+client8.login(process.env.BOT_TOKEN8);
+client9.login(process.env.BOT_TOKEN9);
+client10.login(process.env.BOT_TOKEN10);
+client11.login(process.env.BOT_TOKEN11);
